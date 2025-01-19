d8dbfbae3324509481a79ea32dca3ea870d536e9
manhnguyenduc153 <166704175+manhnguyenduc153@users.noreply.github.com>
2025-01-19 17:38:30 +0700

    DBContext and file jsp

--- ChildrenCare/src/java/dal/DBContext.java
+++ ChildrenCare/src/java/dal/DBContext.java
@@ -4,10 +4,32 @@
  */
 package dal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author FPTSHOP
  */
 public abstract class DBContext {
+
     protected Connection connection;
+
+    public DBContext() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/swp391";
+            String username = "root";
+            String password = "123456";
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
