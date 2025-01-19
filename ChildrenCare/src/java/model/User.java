0d88ac56ee495143ec84ae3df5b0b6ff4ef30c6e
manhnguyenduc153 <166704175+manhnguyenduc153@users.noreply.github.com>
2025-01-19 20:51:08 +0700

    Test

---
+++ ChildrenCare/src/java/model/User.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author FPTSHOP
+ */
+public class User {
+    private int id;
+    private String fullname;
+
+    public User(int id, String fullname) {
+        this.id = id;
+        this.fullname = fullname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+}
